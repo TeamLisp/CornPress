663cfaf9006b2d08da6d1e9ff8d60098c578985c
G.Bence <csopi1996@gmail.com>
2018-04-23 21:32:41 +0200

    Compressor moved to class, multy file compress

--- basic_conpress.py
+++ basic_conpress.py
@@ -1,9 +1,26 @@
 import zstd
 import sys
+import re
 
-infile = sys.argv[1]
-outfile = sys.argv[2]
+class ZstdCompresser:
+	def __init__(self):
+		self.cctx = zstd.ZstdCompressor()
 
-cctx = zstd.ZstdCompressor()
-with open(infile,'rb') as ifh, open(outfile,'wb') as ofh:
-	cctx.copy_stream(ifh,ofh)
+	@staticmethod
+	def to_zstd(base):
+		return re.sub("\..*","",base)
+
+	def lets_press(self, infile, outfile):
+		with open(infile,'rb') as ifh, open(outfile,'wb') as ofh:
+			self.cctx.copy_stream(ifh,ofh)
+
+	def lets_multy(self, *argv):
+		for elem in argv :
+			with open(elem,'rb') as ifh, open(ZstdCompresser.to_zstd(elem), 'wb') as ofh:
+				self.cctx.copy_stream(ifh,ofh)
+
+
+
+compr = ZstdCompresser()
+compr.lets_press("infile.txt","infile.zstd")
+compr.lets_multy("out.me","compress.me","good.me")
