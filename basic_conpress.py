4641640c6818b2364813c9b3ba8ead6465a03199
G.Bence <csopi1996@gmail.com>
2018-04-29 14:04:19 +0200

    Compressing and Decompressing rdy for integration to UI

--- basic_conpress.py
+++ basic_conpress.py
@@ -2,6 +2,7 @@ import zstd
 import sys
 import re
 
+
 class ZstdCompresser:
 	def __init__(self):
 		self.cctx = zstd.ZstdCompressor()
@@ -10,17 +11,21 @@ class ZstdCompresser:
 	def to_zstd(base):
 		return re.sub("\..*","",base)
 
-	def lets_press(self, infile, outfile):
-		with open(infile,'rb') as ifh, open(outfile,'wb') as ofh:
+	def lets_press(self, infile):
+		with open(infile,'rb') as ifh, open(ZstdCompresser.to_zstd(infile) + ".zstd",'wb') as ofh:
 			self.cctx.copy_stream(ifh,ofh)
 
 	def lets_multy(self, *argv):
 		for elem in argv :
-			with open(elem,'rb') as ifh, open(ZstdCompresser.to_zstd(elem), 'wb') as ofh:
+			with open(elem,'rb') as ifh, open(ZstdCompresser.to_zstd(elem) + ".zstd" , 'wb') as ofh:
 				self.cctx.copy_stream(ifh,ofh)
 
 
 
 compr = ZstdCompresser()
-compr.lets_press("infile.txt","infile.zstd")
-compr.lets_multy("out.me","compress.me","good.me")
+
+if len(sys.argv) > 2 :
+	compr.lets_multy(*sys.argv)
+else :
+	compr.lets_press(sys.argv[1])
+
--- basic_decompress.py
+++ basic_decompress.py
@@ -1,9 +1,30 @@
 import zstd
 import sys
+import re
 
-infile = sys.argv[1]
-outfile = sys.argv[2]
 
-cctx = zstd.ZstdDecompressor()
-with open(infile,'rb') as ifh, open(outfile,'wb') as ofh:
-	cctx.copy_stream(ifh,ofh)
+class ZstdDecomp:
+	def __init__(self):
+		self.cctx = zstd.ZstdDecompressor()
+
+	@staticmethod
+	def to_zstd(base):
+		return re.sub("\..*","",base)
+
+	def lets_decomp(self, infile):
+		with open(infile,'rb') as ifh, open(ZstdDecomp.to_zstd(infile),'wb') as ofh:
+			self.cctx.copy_stream(ifh,ofh)
+
+	def lets_multy(self, *argv):
+		for elem in argv :
+			with open(elem,'rb') as ifh, open(ZstdDecomp.to_zstd(elem), 'wb') as ofh:
+				self.cctx.copy_stream(ifh,ofh)
+
+
+
+compr = ZstdDecomp()
+
+if len(sys.argv) > 2 :
+	compr.lets_multy(*sys.argv)
+else :
+	compr.lets_decomp(sys.argv[1])
